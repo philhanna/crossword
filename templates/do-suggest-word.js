88fc21c6b658b11c67637547680680f459e822ff
Phil Hanna <ph1204@gmail.com>
2020-06-01 00:05:43 -0400

    First draft of fix for issue #1 - suggest word

--- templates/do-suggest-word.js
+++ templates/do-suggest-word.js
@@ -1,6 +1,5 @@
 /* Suggest a word that matches the pattern */
 function do_suggest_word() {
-
     // Get the pattern
     var elem_word = document.getElementById('edit-word-dialog-word');
     var pattern = elem_word.value;
@@ -11,15 +10,18 @@ function do_suggest_word() {
        if (this.readyState == 4 && this.status == 200) {
           jsonstr = this.responseText;
           words = JSON.parse(jsonstr);
-          msg = "";
-          for (int i = 0; i < words.length; i++) {
-            msg += "\n" + words[i];
+          elem_select = document.getElementById('edit-word-wordlist-select')
+          options = ""
+          for (var i = 0; i < words.length; i++) {
+            word = words[i]
+            options += "<option value='" + word + "'>" + word + "</option>"
           }
-          alert("DEBUG: " + msg);
+          elem_select.innerHTML = options
+          document.getElementById('edit-word-wordlist').style.display = 'block';
        }
    }
    var url = '{{ url_for("wordlists")}}';
+   url += "?pattern=" + pattern
    xhttp.open("GET", url, true);
    xhttp.send();
-
 }
