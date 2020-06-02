bf2c8319be7084510929bf36ac0d98efae6f628a
Phil Hanna <ph1204@gmail.com>
2020-06-02 19:37:30 -0400

    Added unit test for importing an incompatible clue spreadsheet
    Need to surround call with try: except RuntimeError

--- test/test_import_clues.py
+++ test/test_import_clues.py
@@ -20,3 +20,19 @@ class TestImportClues(TestCase):
         expected = "...and it feels so nice"
         actual = puzzle.get_across_word(13).get_clue()
         self.assertEqual(expected, actual)
+
+    def test_incompatible_puzzle(self):
+        with StringIO() as fp:
+            fp.write("seq,direction,word,clue" + "\n")
+            fp.write("44,across,REUNITED,...and it feels so nice" + "\n")
+            csvstr = fp.getvalue()
+
+        visitor = ClueImportVisitor(csvstr)
+        puzzle = TestPuzzle.create_solved_atlantic_puzzle()
+        try:
+            puzzle.accept(visitor)
+            expected = "...and it feels so nice"
+            actual = puzzle.get_across_word(44).get_clue()
+            self.assertEqual(expected, actual)
+        except RuntimeError:
+            pass # This was expected
