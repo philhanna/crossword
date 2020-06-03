8367bc1755c6d489ea2cac23770904e9b045fc02
Phil Hanna <ph1204@gmail.com>
2020-06-02 22:24:28 -0400

    Fixed issue#16

--- templates/grid-chooser-ajax.js
+++ templates/grid-chooser-ajax.js
@@ -1,19 +1,36 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
- if (this.readyState == 4 && this.status == 200) {
-    jsonstr = this.responseText
-    grid_list = JSON.parse(jsonstr);
-    links = "";
-    for (i = 0; i < grid_list.length; i++) {
-        filename = grid_list[i]
-        href = build_url(filename)
-        quoted_href = "'" + href + "'"
-        anchor = "<a href=" + quoted_href + ">" + filename + "</a>"
-        listitem = "<li>" + anchor + "</li>"
-        links += listitem + "\n"
-    }
-    document.getElementById("grid-list").innerHTML = links
- }
+   if (this.readyState == 4 && this.status == 200) {
+
+      // Get the JSON array of grid names returned by the AJAX call
+      jsonstr = this.responseText;
+      grid_list = JSON.parse(jsonstr);
+
+      // Clear out the <ul> that will contain the list items
+      elem_list = document.getElementById('grid-list');
+      elem_list.innerHTML = ""
+
+      // Populate the list
+      for (i = 0; i < grid_list.length; i++) {
+
+         // Get the next file name in the list
+         gridname = grid_list[i];
+
+         // Create an <a> element for the URL
+         // with the full URL that will be used as the link
+         elem_anchor = document.createElement("a");
+         elem_anchor.href = build_url(gridname);
+         elem_anchor.style.textDecoration = "none"; // No underline
+         elem_anchor.appendChild(document.createTextNode(gridname));
+
+         // Create a <li> to contain the <a>
+         elem_list_item = document.createElement("li");
+         elem_list_item.appendChild(elem_anchor);
+
+         // and append the list item to the <ul>
+         elem_list.appendChild(elem_list_item);
+      }
+   }
 };
 var url = '{{ url_for("grids")}}';
 xhttp.open("GET", url, true);
--- templates/puzzle-chooser-ajax.js
+++ templates/puzzle-chooser-ajax.js
@@ -1,19 +1,36 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
- if (this.readyState == 4 && this.status == 200) {
-    jsonstr = this.responseText
-    puzzle_list = JSON.parse(jsonstr);
-    links = "";
-    for (i = 0; i < puzzle_list.length; i++) {
-        filename = puzzle_list[i]
-        href = build_url(filename)
-        quoted_href = "'" + href + "'"
-        anchor = "<a href=" + quoted_href + ">" + filename + "</a>"
-        listitem = "<li>" + anchor + "</li>"
-        links += listitem + "\n"
-    }
-    document.getElementById("puzzle-list").innerHTML = links
- }
+   if (this.readyState == 4 && this.status == 200) {
+
+      // Get the JSON array of puzzle names returned by the AJAX call
+      jsonstr = this.responseText
+      puzzle_list = JSON.parse(jsonstr);
+
+      // Clear the <ul> that will contain the list items
+      elem_list = document.getElementById("puzzle-list");
+      elem_list.innerHTML = ""
+
+      // Populate the list
+      for (i = 0; i < puzzle_list.length; i++) {
+
+         // Get the next puzzle name in the list
+         puzzlename = puzzle_list[i];
+
+         // Create an <a> element for the URL
+         // with the full URL that will be used as the link
+         elem_anchor = document.createElement("a");
+         elem_anchor.href = build_url(puzzlename);
+         elem_anchor.style.textDecoration = "none"; // No underline
+         elem_anchor.appendChild(document.createTextNode(puzzlename));
+
+         // Create a <li> to contain the <a>
+         elem_list_item = document.createElement("li");
+         elem_list_item.appendChild(elem_anchor);
+
+         // and append the list item to the <ul>
+         elem_list.appendChild(elem_list_item);
+      }
+   }
 };
 var url = '{{ url_for("puzzles")}}';
 xhttp.open("GET", url, true);
