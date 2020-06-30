1eb4cb9d59b1c090d75646dd773b4791e295c9ca
Phil Hanna <ph1204@gmail.com>
2020-06-30 17:12:11 -0400

    Removed the padding in the clue list

--- crossword/ui/uipuzzle.py
+++ crossword/ui/uipuzzle.py
@@ -20,11 +20,11 @@ def puzzle_screen():
     # Get the clues
     clues = {
         "across": [
-            {"seq": seq, "text": word.get_clue() or "\xA0"*100}
+            {"seq": seq, "text": word.get_clue() or ""}
             for seq, word in puzzle.across_words.items()
         ],
         "down": [
-            {"seq": seq, "text": word.get_clue() or "\xA0"*100}
+            {"seq": seq, "text": word.get_clue() or ""}
             for seq, word in puzzle.down_words.items()
         ],
     }
