63b024a3eaa37992fd0e07ada1616b18c01ba4b5
Phil Hanna <ph1204@gmail.com>
2020-06-30 16:08:36 -0400

    Bumped version number to 2.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='crossword',
-      version='2.1.4',
+      version='2.2.0',
       description='Crossword Puzzle Editor',
       url='http://github.com/philhanna/crossword',
       author='Phil Hanna',
