c89e53a4778d19c79ddd7680c7b21121b95f48a0
Phil Hanna <ph1204@gmail.com>
2020-07-12 12:05:22 -0400

    Added unit tests for crossword.__init__.py

---
+++ crossword/tests/test_package.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from datetime import datetime
+from time import sleep
+
+from crossword import get_elapsed_time, dbfile
+
+
+class TestPackage(unittest.TestCase):
+
+    def test_get_elapsed_time(self):
+        stime = datetime.now()
+        sleep(0.5)
+        etime = datetime.now()
+        expected = 0.5
+        actual = get_elapsed_time(stime, etime)
+        # 50 milliseconds difference is good enough
+        self.assertAlmostEqual(expected, actual, delta=0.05)
+
+    def test_dbfile(self):
+        db = dbfile()
+        self.assertTrue(os.path.exists(db))
