16d16ecff766a26c7a4ef155f841af73a06b4a1b
Phil Hanna <ph1204@gmail.com>
2020-07-21 15:58:31 -0400

    Removed debugging print statement

--- crossword/tests/test_puzzle_to_xml.py
+++ crossword/tests/test_puzzle_to_xml.py
@@ -10,13 +10,4 @@ class TestPuzzleToXML(TestCase):
         user = MockUser()
         puzzle = TestPuzzle.create_nyt_daily()
         app = PuzzleToXML(user, puzzle)
-        print(app.xmlstr)
         self.assertTrue('<clue number="50" word="67">Lollipop</clue>' in app.xmlstr, app.xmlstr)
-
-    # Skip this unless writing a new version to Dropbox
-    def notest_xml_temp_write(self):
-        user = MockUser()
-        puzzle = TestPuzzle.create_nyt_daily()
-        app = PuzzleToXML(user, puzzle)
-        with open("/home/saspeh/Dropbox/crossword/acts_mine.xml", "w") as fp:
-            fp.write(app.xmlstr)
