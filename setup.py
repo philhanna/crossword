0a8db2660feb983ccc88999dba072732c24e80ce
Phil Hanna <ph1204@gmail.com>
2020-08-01 17:40:08 -0400

    Fix for issue #124

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='crossword',
-      version='2.3.2',
+      version='2.4.0',
       description='Crossword Puzzle Editor',
       url='http://github.com/philhanna/crossword',
       author='Phil Hanna',
