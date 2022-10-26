0209ef339104b89a0b09635815dbcd99b4474b9a
Phil Hanna <ph1204@gmail.com>
2022-10-26 05:15:58 -0400

    Deleted duplicate unit test

--- tests/cells/test_numbered_cell_json.py
+++ tests/cells/test_numbered_cell_json.py
@@ -10,48 +10,46 @@ class TestNumberedCellJSON(TestCase):
     def test_just_r_and_c(self):
         numbered_cell = NumberedCell(1, 3, 2)
         jsonstr = json.dumps(numbered_cell, cls=CrosswordJSONEncoder)
-        obj = json.loads(jsonstr, cls=CrosswordJSONDecoder)
-        self.assertEqual(numbered_cell, obj)
-        self.assertEqual(0, obj.a)
-        self.assertEqual(0, obj.d)
+        actual = json.loads(jsonstr, cls=CrosswordJSONDecoder)
+        self.assertEqual(numbered_cell, actual)
+        self.assertEqual(0, actual.a)
+        self.assertEqual(0, actual.d)
 
     def test_with_a(self):
         numbered_cell = NumberedCell(1, 3, 2, a=4)
-        expected = "NumberedCell(seq=1,r=3,c=2,a=4)"
-        actual = str(numbered_cell)
-        self.assertEqual(expected, actual)
-        self.assertEqual(4, numbered_cell.a)
-        self.assertEqual(0, numbered_cell.d)
+        jsonstr = json.dumps(numbered_cell, cls=CrosswordJSONEncoder)
+        actual = json.loads(jsonstr, cls=CrosswordJSONDecoder)
+        self.assertEqual(numbered_cell, actual)
+        self.assertEqual(4, actual.a)
+        self.assertEqual(0, actual.d)
 
     def test_with_d(self):
         numbered_cell = NumberedCell(45, 3, 2, d=46)
-        expected = "NumberedCell(seq=45,r=3,c=2,d=46)"
-        actual = str(numbered_cell)
-        self.assertEqual(expected, actual)
-        self.assertEqual(0, numbered_cell.a)
-        self.assertEqual(46, numbered_cell.d)
+        jsonstr = json.dumps(numbered_cell, cls=CrosswordJSONEncoder)
+        actual = json.loads(jsonstr, cls=CrosswordJSONDecoder)
+        self.assertEqual(numbered_cell, actual)
+        self.assertEqual(0, actual.a)
+        self.assertEqual(46, actual.d)
 
     def test_with_both(self):
         numbered_cell = NumberedCell(22, 3, 2, d=46, a=12)
-        expected = "NumberedCell(seq=22,r=3,c=2,a=12,d=46)"
-        actual = str(numbered_cell)
-        self.assertEqual(expected, actual)
-        self.assertEqual(12, numbered_cell.a)
-        self.assertEqual(46, numbered_cell.d)
+        jsonstr = json.dumps(numbered_cell, cls=CrosswordJSONEncoder)
+        actual = json.loads(jsonstr, cls=CrosswordJSONDecoder)
+        self.assertEqual(numbered_cell, actual)
+        self.assertEqual(12, actual.a)
+        self.assertEqual(46, actual.d)
 
     def test_with_both_as_positional(self):
         numbered_cell = NumberedCell(4, 3, 2, 12, 46)
-        expected = "NumberedCell(seq=4,r=3,c=2,a=12,d=46)"
-        actual = str(numbered_cell)
-        self.assertEqual(expected, actual)
-        self.assertEqual(12, numbered_cell.a)
-        self.assertEqual(46, numbered_cell.d)
+        jsonstr = json.dumps(numbered_cell, cls=CrosswordJSONEncoder)
+        actual = json.loads(jsonstr, cls=CrosswordJSONDecoder)
+        self.assertEqual(numbered_cell, actual)
+        self.assertEqual(12, actual.a)
+        self.assertEqual(46, actual.d)
 
     def test_equals_when_equal(self):
         nca = NumberedCell(17, 3, 2, 4, 5)
+        jsonstra = json.dumps(nca, cls=CrosswordJSONEncoder)
         ncb = NumberedCell(17, 3, 2, 3 + 1, 4 + 1)
-        self.assertEqual(nca, ncb)
-
-    def test_hash(self):
-        nc1 = NumberedCell(4, 3, 2, 12, 46)
-        self.assertIsNotNone(hash(nc1))
+        jsonstrb = json.dumps(ncb, cls=CrosswordJSONEncoder)
+        self.assertEqual(jsonstra, jsonstrb)
